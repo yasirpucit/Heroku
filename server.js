a2834b3e96ddf3d371fef5a097890dc165a38da8
yasirpucit <59232790+yasirpucit@users.noreply.github.com>
2021-02-18 16:16:39 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+
 const express = require("express");
 const app = express();
+const port = process.env.port || 3000;
 
-
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server Is Listening");
 })
 
+const data = (req,res) => {
+    res.send("Started listening on heroku");
+    console.log("Heroku App Deployed");
+}
 
-app.use("/", "App deployed on Heroku");
+
+app.use("/", data);
